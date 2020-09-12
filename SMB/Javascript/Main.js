11bde3588d7334c73e00f5d76b2b0c4c21a9041f
Алексей Гасюк <alekseygasyuk50@gmail.com>
2020-09-12 21:04:47 +0300

    Реализовано модальное окно и адаптировано под мобильные устройства

--- SMB/Javascript/Main.js
+++ SMB/Javascript/Main.js
@@ -1,11 +1,13 @@
 ﻿function confirmDelete(argumentName, id, path) {
     let modalWindow = document.querySelector(".modalWindow")
     modalWindow.style.display = "block"
-    modalWindow.innerHTML = `<form method="post" action="${path}">
+    modalWindow.innerHTML = `
+                            <b class="modalWindowText">Вы уверены, что хотите удалить это?</b>
+                            <form class="modalWindowButtons" method="post" action="${path}">
                                 <input type="hidden" name="${argumentName}" value="${id}" />
-                                <input onclick="closeModalWindow()" type="submit" value="Удалить" />
-                            </form >
-                            <button onclick="closeModalWindow()">Назад</button>`
+                                <input id="deleteButton" onclick="closeModalWindow()" type="submit" value="Удалить" />
+                                <button type="button" id="backButton" onclick="closeModalWindow()">Назад</button>
+                            </form >`
 }
 
 function closeModalWindow() {
