af56e7d9ee243707610927d4461cbe48d50f8cd6
Алексей Гасюк <alekseygasyuk50@gmail.com>
2020-10-25 15:57:26 +0300

    Финальные поправки

---
+++ SMB/Models/VirtualPathCorrector.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Hosting;
+
+namespace SMB.Models
+{
+    public static class VirtualPathCorrector
+    {
+        public static string CorrectVirtualPath => HostingEnvironment.ApplicationVirtualPath == "/" ?
+                                                   HostingEnvironment.ApplicationVirtualPath :
+                                                   HostingEnvironment.ApplicationVirtualPath + "/";
+    }
+}
